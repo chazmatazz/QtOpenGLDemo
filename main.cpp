62d25b5e91b7f010f1ffeb0b6afe4e033119330c
Charles Dietrich <charles.m.dietrich@gmail.com>
2011-04-07 12:57:31 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// http://programanddesign.com/cpp/qt-opengl-code-example/
+
+#include <QtGui/QApplication>
+#include <QtOpenGL/QGLWidget>
+#include "GLWidget.h"
+
+int main(int argc, char *argv[]) {
+    
+    QApplication app(argc, argv);
+    
+    GLWidget window;
+    window.resize(800,600);
+    window.show();
+    
+    return app.exec();
+}
